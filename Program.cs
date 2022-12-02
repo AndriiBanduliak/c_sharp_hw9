e55297ffed6b92df29ed68d15ccae6056ca3f901
Andrii Banduliak <banduliak1303@gmail.com>
2022-12-02 22:11:42 +0300

    3d task

--- Program.cs
+++ Program.cs
@@ -4,30 +4,21 @@ Clear();
 
 int m = InputNumbers("Enter m: ");
 int n = InputNumbers("Enter n: ");
-int temp = m;
 
-if (m > n) 
+int functionAkkerman = Ack(m, n);
+
+Write($"Function of Akkerman is  {functionAkkerman} ");
+
+int Ack(int m, int n)
 {
-  m = n; 
-  n = temp;
-}
-
-PrintSumm(m, n, temp=0);
-
-void PrintSumm(int m, int n, int summ)
-{
-  summ = summ + n;
-  if (n <= m)
-  {
-    Write($"Sum of elements is {summ} ");
-    return;
-  }
-  PrintSumm(m, n - 1, summ);
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
 }
 
 int InputNumbers(string input) 
 {
   Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
+  int output = Convert.ToInt32(ReadLine());
   return output;
 }
